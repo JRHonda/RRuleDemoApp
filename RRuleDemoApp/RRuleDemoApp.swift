df38c6370595b57a52e44704655d90aa0825576e
Justin Honda <justinohonda@gmail.com>
2022-07-03 21:35:46 -0400

    Initial Commit

---
+++ RRuleDemoApp/RRuleDemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  RRuleDemoAppApp.swift
+//  RRuleDemoApp
+//
+//  Created by Justin Honda on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct RRuleDemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
